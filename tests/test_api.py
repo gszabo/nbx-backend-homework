225840516b5c43ba8d86fd7437f5f0300fc3b082
Gabor Szabo <harry.bme@gmail.com>
2020-07-18 14:57:23 +0200

    test for getting individual user

--- tests/test_api.py
+++ tests/test_api.py
@@ -15,6 +15,7 @@ def client(loop, aiohttp_client):
 async def delete_users(client):
     await client.delete("/users")
 
+
 async def test_healthcheck(client):
     resp = await client.get("/")
     assert resp.status == 200
@@ -48,3 +49,33 @@ class TestListUsers:
         assert resp.status == 200
         body = await resp.json()
         assert [] == body
+
+
+class TestCreateUser:
+    async def test_response_contains_given_data(self, client):
+        input_data = {"name": "User_A", "email": "email_a@example.com"}
+
+        create_response = await client.post("/users", json=input_data)
+        response_data = await create_response.json()
+
+        assert response_data["name"] == input_data["name"]
+        assert response_data["email"] == input_data["email"]
+
+
+class TestGetUser:
+    async def test_user_not_found_responds_with_404(self, client):
+        resp = await client.get("/users/nouser")
+
+        assert resp.status == 404
+
+    async def test_user_exists_responds_with_user_data(self, client):
+        create_response = await client.post(
+            "/users", json={"name": "User_A", "email": "email_a@example.com"}
+        )
+        user_data = await create_response.json()
+
+        get_user_response = await client.get("/users/" + user_data["id"])
+        get_user_data = await get_user_response.json()
+
+        assert get_user_response.status == 200
+        assert get_user_data == user_data
