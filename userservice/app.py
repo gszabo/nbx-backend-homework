c985e4fde3a404805e1a64d3152d1378dbccb752
Gabor Szabo <harry.bme@gmail.com>
2020-07-18 11:00:51 +0200

    create and list users

--- userservice/app.py
+++ userservice/app.py
@@ -1,11 +1,15 @@
 import logging
 from aiohttp import web
+import uuid
 
 
 LOGGER = logging.getLogger(__name__)
 
 
 routes = web.RouteTableDef()
+
+
+users = {}
 
 
 @routes.get('/')
@@ -15,12 +19,16 @@ async def health(request):
 
 @routes.get('/users')
 async def get_users(request):
-    return web.json_response([])
+    return web.json_response(list(users.values()))
 
 
 @routes.post('/users')
 async def create_user(request):
-    return web.json_response({}, status=201)
+    body = await request.json()
+    id_ = uuid.uuid4()
+    new_user = {"name": body["name"], "id": str(id_), "email": body["email"]}
+    users[id_] = new_user
+    return web.json_response(new_user, status=201)
 
 
 @routes.put('/users/{user_id}')
