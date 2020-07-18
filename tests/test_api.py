07808a89ec3df92de541a6bb1ec0a04ae1520a50
Gabor Szabo <harry.bme@gmail.com>
2020-07-18 15:32:59 +0200

    tests for update user endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -79,3 +79,67 @@ class TestGetUser:
 
         assert get_user_response.status == 200
         assert get_user_data == user_data
+
+
+class TestUpdateUser:
+    async def test_user_not_found_responds_with_404(self, client):
+        resp = await client.put("/users/nouser", json={})
+
+        assert resp.status == 404
+
+    async def test_update_name(self, client):
+        create_response = await client.post(
+            "/users", json={"name": "User_A", "email": "email_a@example.com"}
+        )
+        user_data = await create_response.json()
+
+        update_user_response = await client.put(
+            "/users/" + user_data["id"], json={"name": "Renamed_User_A"}
+        )
+
+        assert update_user_response.status == 200
+        updated_user_data = await update_user_response.json()
+
+        get_user_response = await client.get("/users/" + user_data["id"])
+        get_user_data = await get_user_response.json()
+
+        assert updated_user_data == get_user_data
+        assert updated_user_data["name"] == "Renamed_User_A"
+        assert updated_user_data["email"] == "email_a@example.com"
+
+    async def test_update_email(self, client):
+        create_response = await client.post(
+            "/users", json={"name": "User_A", "email": "email_a@example.com"}
+        )
+        user_data = await create_response.json()
+
+        update_user_response = await client.put(
+            "/users/" + user_data["id"], json={"email": "email_a2@example.com"}
+        )
+
+        assert update_user_response.status == 200
+        updated_user_data = await update_user_response.json()
+
+        get_user_response = await client.get("/users/" + user_data["id"])
+        get_user_data = await get_user_response.json()
+
+        assert updated_user_data == get_user_data
+        assert updated_user_data["name"] == "User_A"
+        assert updated_user_data["email"] == "email_a2@example.com"
+
+    async def test_id_and_other_fields_are_ignored(self, client):
+        create_response = await client.post(
+            "/users", json={"name": "User_A", "email": "email_a@example.com"}
+        )
+        original_user_data = await create_response.json()
+
+        update_user_response = await client.put(
+            "/users/" + original_user_data["id"], json={"id": "id42", "age": 42}
+        )
+        assert update_user_response.status == 200
+        updated_user_data = await update_user_response.json()
+
+        assert updated_user_data == original_user_data
+
+        other_id_response = await client.get("/users/id42")
+        assert other_id_response.status == 404
